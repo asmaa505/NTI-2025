4b5e75b6cd43fb95492b42f5f43674b7e91c5b49
Asmaa Maryah <asmaaahmedt@gmail.com>
2025-07-14 01:05:00 +0300

    session 6 animation & intro to JS

---
+++ session-6/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+var asmaa={
+    name : "asmaa maryah",
+    age : 20,
+    course : "frontend",
+    review : " course content is HTNL , CSS , JS and react ",
+}
+
+console.log(asmaa);
+console.log(asmaa.age);
+

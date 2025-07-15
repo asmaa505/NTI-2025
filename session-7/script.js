0d2775836e148e2e0131b32fd1a660da85495416
Asmaa Maryah <asmaaahmedt@gmail.com>
2025-07-16 00:38:47 +0300

    operators , control flow(if , switch),array

---
+++ session-7/script.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+// let num = Math.random()* 1000
+// console.log(Math.round(num))
+
+
+// let x = 20;
+// if( x >= 65 && x <= 100)
+// {
+//     console.log(" OLD people")
+// }else if( x >= 20 && x <= 64){
+//     console.log(" ADULT")
+// }else if( x >= 0 && x <= 19)
+// {
+//     console.log(" CHILD")
+// }else{
+//     console.log(" not valid age!")
+// }
+
+
+// let day = "sun";
+// switch(day)
+// {
+//     case "sun":
+//     case "mon":
+//     case "tue":
+//     case "wed":
+//     case "thu":
+//         console.log("workkkk!");
+//         break;
+//     case "fri":
+//     case "sat":
+//         console.log("weekend")
+//         break;
+//     default:
+//         console.log("invalid day!!");
+
+// }
+
+
+// let name = window.prompt("please, enter your name")
+// let age = window.prompt("please, enter your age")
+// document.write(`your name is: ${name} <br><hr> your age is: ${age + 10}` )
+
+
+
+
+// let month = window.prompt("enter the month")
+
+// if(month >= 1 && month <= 3)
+// {
+//     document.write(`winter perid`)
+// }else if(month >= 4 && month <= 7)
+// {
+//     document.write(`autom perid`)
+// }else if(month >= 8 && month <= 11)
+// {
+//     document.write("summer period")
+// }else if(month >= 12 & month <= 15){
+//     document.write("spring period")
+// }else{
+//     document.write("invalid input")
+// }
+
+
+let arr = [1 , 2 , 3 , 4];
+let l = arr.pop()   // 4
+let f = arr.shift()  // 1
+// console.log(arr)
+
+arr.push(f)
+arr.unshift(l)
+console.log(arr)
+
+
